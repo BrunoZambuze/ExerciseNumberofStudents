a8bfe3782cfe17297c52d0503b1947a470daad0f
BrunoZambuze <zambuzesilva@hotmail.com>
2023-11-28 10:12:14 -0300

    ExerciseNumberofStudents

---
+++ ExerciseNumberofStudents/Domain/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExerciseNumberofStudents.Domain
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+
+        public Student()
+        {
+        }
+
+        public Student(int id)
+        {
+            Id = id;
+        }
+
+        //Imprimir os dados
+        public override string ToString()
+        {
+            return $"-{Id}";
+        }
+
+        public void AddStudent(HashSet<Student> student, int id)
+        {
+            student.Add(new Student(id));
+        }
+    }
+}
